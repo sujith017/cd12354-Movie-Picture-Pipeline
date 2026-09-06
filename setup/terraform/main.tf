8db0da05004a004cd12619604fcac6c5aef17fe2
sujith017 <sujithb934@gmail.com>
2026-09-06 08:28:46 +0530

    updated the setup

--- setup/terraform/main.tf
+++ setup/terraform/main.tf
@@ -6,6 +6,7 @@ resource "aws_vpc" "vpc" {
   tags = {
     "Name" = "udacity"
   }
+
   cidr_block           = "10.0.0.0/16"
   enable_dns_support   = true
   enable_dns_hostnames = true
@@ -22,6 +23,7 @@ resource "aws_subnet" "public_subnet" {
   cidr_block              = "10.0.1.0/24"
   availability_zone       = "us-east-1${var.public_az}"
   map_public_ip_on_launch = true
+
   tags = {
     Name = "udacity-public"
   }
@@ -52,6 +54,7 @@ resource "aws_subnet" "private_subnet" {
   vpc_id            = aws_vpc.vpc.id
   availability_zone = "us-east-1${var.private_az}"
   cidr_block        = "10.0.2.0/24"
+
   tags = {
     Name = "udacity-private"
   }
@@ -74,7 +77,7 @@ resource "aws_route_table_association" "private" {
 
 # Create EKS endpoint for private access
 resource "aws_vpc_endpoint" "eks" {
-  count               = var.enable_private == true ? 1 : 0 # only enable when private
+  count               = var.enable_private == true ? 1 : 0
   vpc_id              = aws_vpc.vpc.id
   service_name        = "com.amazonaws.us-east-1.eks"
   vpc_endpoint_type   = "Interface"
@@ -144,14 +147,18 @@ resource "aws_eks_cluster" "main" {
   name     = "cluster"
   version  = var.k8s_version
   role_arn = aws_iam_role.eks_cluster.arn
+
   vpc_config {
     subnet_ids              = [aws_subnet.private_subnet.id, aws_subnet.public_subnet.id]
     endpoint_public_access  = var.enable_private == true ? false : true
     endpoint_private_access = true
   }
-  depends_on = [aws_iam_role_policy_attachment.eks_cluster, aws_iam_role_policy_attachment.eks_service]
-}
 
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_cluster,
+    aws_iam_role_policy_attachment.eks_service
+  ]
+}
 
 # Create an IAM role for the EKS cluster
 resource "aws_iam_role" "eks_cluster" {
@@ -159,10 +166,12 @@ resource "aws_iam_role" "eks_cluster" {
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
+
     Statement = [
       {
         Action = "sts:AssumeRole"
         Effect = "Allow"
+
         Principal = {
           Service = "eks.amazonaws.com"
         }
@@ -182,21 +191,25 @@ resource "aws_iam_role_policy_attachment" "eks_service" {
   role       = aws_iam_role.eks_cluster.name
 }
 
-
 ##################
 # EKS Node Group
 ##################
-# Track latest release for the given k8s version
+# Track latest Amazon Linux 2023 release for the given Kubernetes version
 data "aws_ssm_parameter" "eks_ami_release_version" {
-  name = "/aws/service/eks/optimized-ami/${aws_eks_cluster.main.version}/amazon-linux-2/recommended/release_version"
+  name = "/aws/service/eks/optimized-ami/${aws_eks_cluster.main.version}/amazon-linux-2023/x86_64/standard/recommended/release_version"
 }
 
 resource "aws_eks_node_group" "main" {
   node_group_name = "udacity"
   cluster_name    = aws_eks_cluster.main.name
   version         = aws_eks_cluster.main.version
+  ami_type        = "AL2023_x86_64_STANDARD"
   node_role_arn   = aws_iam_role.node_group.arn
-  subnet_ids      = [var.enable_private == true ? aws_subnet.private_subnet.id : aws_subnet.public_subnet.id]
+
+  subnet_ids = [
+    var.enable_private == true ? aws_subnet.private_subnet.id : aws_subnet.public_subnet.id
+  ]
+
   release_version = nonsensitive(data.aws_ssm_parameter.eks_ami_release_version.value)
   instance_types  = ["t3.small"]
 
@@ -206,9 +219,8 @@ resource "aws_eks_node_group" "main" {
     min_size     = 1
   }
 
-
-  # Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling.
-  # Otherwise, EKS will not be able to properly delete EC2 Instances and Elastic Network Interfaces.
+  # Ensure that IAM Role permissions are created before and deleted after
+  # EKS Node Group handling.
   depends_on = [
     aws_iam_role_policy_attachment.node_group_policy,
     aws_iam_role_policy_attachment.cni_policy,
@@ -220,7 +232,9 @@ resource "aws_eks_node_group" "main" {
   }
 }
 
-// IAM Configuration
+################
+# IAM Configuration
+################
 resource "aws_iam_role" "node_group" {
   name               = "udacity-node-group"
   assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
@@ -245,6 +259,7 @@ data "aws_iam_policy_document" "assume_role_policy" {
   statement {
     effect  = "Allow"
     actions = ["sts:AssumeRole"]
+
     principals {
       type        = "Service"
       identifiers = ["ec2.amazonaws.com"]
@@ -261,6 +276,7 @@ resource "aws_codebuild_project" "codebuild" {
   description   = "Udacity CodeBuild project"
   service_role  = aws_iam_role.codebuild.arn
   build_timeout = 60
+
   artifacts {
     type = "NO_ARTIFACTS"
   }
@@ -285,40 +301,38 @@ resource "aws_codebuild_project" "codebuild" {
   }
 }
 
-# Create the Codebuild Role
+# Create the CodeBuild Role
 resource "aws_iam_role" "codebuild" {
   name = "codebuild-role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
+
     Statement = [
       {
         Effect = "Allow"
+
         Principal = {
           Service = "codebuild.amazonaws.com"
         }
+
         Action = "sts:AssumeRole"
       }
     ]
   })
 }
 
-# Attach the IAM policy to the codebuild role
+# Attach the IAM policy to the CodeBuild role
 resource "aws_iam_role_policy_attachment" "codebuild" {
   policy_arn = "arn:aws:iam::aws:policy/AWSCodeBuildAdminAccess"
   role       = aws_iam_role.codebuild.name
 }
 
 ####################
-# Github Action role
+# GitHub Action role
 ####################
 resource "aws_iam_user" "github_action_user" {
   name = "github-action-user"
-}
-
-resource "aws_iam_user_policy" "github_action_user_permission" {
-  user   = aws_iam_user.github_action_user.name
-  policy = data.aws_iam_policy_document.github_policy.json
 }
 
 data "aws_iam_policy_document" "github_policy" {
--- setup/terraform/versions.tf
+++ setup/terraform/versions.tf
@@ -4,10 +4,11 @@ provider "aws" {
 
 terraform {
   required_version = "1.3.9"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "4.55.0"
+      version = ">= 5.0"
     }
   }
 }
